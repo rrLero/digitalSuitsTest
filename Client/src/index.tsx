1968261617d766437de9c760cfe99602cc6da184
Roman Levchenko <roman.levchenko1981@gmail.com>
2021-11-19 21:28:49 +0200

    add client

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router';
+// import { Router } from 'react-router';
 import { Provider } from 'react-redux';
-import { createHashHistory } from 'history';
+// import { createHashHistory } from 'history';
 import App from './App';
 import { store } from './store/store';
 
-const history = createHashHistory();
+// const history = createHashHistory();
 const container = document.getElementById('root');
 
 if (container) {
@@ -14,6 +14,6 @@ if (container) {
     root.render(
         <Provider store={store}>
             <App />
-        </Provider>
+        </Provider>,
     );
 }
